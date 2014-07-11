d3032d9376ac958568159c9ccfccd57fae0d45c7
Matt Cano <cano.matt@gmail.com>
2014-07-10 21:43:42 -0700

    Delete micropost functioning. All tests passing.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
 	before_action :signed_in_user, only: [:create, :destroy]
+	before_action :correct_user, 	 only: [:destroy]
 
 	def index
 	end
@@ -16,6 +17,8 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+  	@micropost.destroy
+  	redirect_to root_url
   end
 
   private
@@ -23,4 +26,10 @@ class MicropostsController < ApplicationController
     def micropost_params
       params.require(:micropost).permit(:content)
     end
+
+	def correct_user
+	  @micropost = current_user.microposts.find(params[:id])
+	rescue
+	  redirect_to root_url
+	end
 end
